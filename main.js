01b06457ba86f7a9514155a6355628a305b5c772
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-06-03 22:29:20 +0100

    Reorder

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as CANNON from "cannon-es";
 
 import * as THREE from "three";
+import { options } from "./options.js";
 
 const canvasEl = document.querySelector("#canvas");
 
@@ -16,59 +17,12 @@ const params = {
   notchDepth: 0.1,
 };
 
-const options = [
-  {
-    value: 1,
-    randomForce: 0.1 + 0.2 * 0.5054330461600753,
-    position: new CANNON.Vec3(0, 0, 0.2),
-  },
-  {
-    value: 2,
-    randomForce: 0.2 + 0.4 * 0.6195434488172735,
-    position: new CANNON.Vec3(0.4, 0, 0.2),
-  },
-  {
-    value: 3,
-    randomForce: 0.2 + 0.4 * 0.6898916672811655,
-    position: new CANNON.Vec3(0.4, 0.8, 0.6),
-  },
-  {
-    value: 4,
-    randomForce: 0.1 + 0.2 * 0.9532962453133706,
-    position: new CANNON.Vec3(0, 0, 0.2),
-  },
-  {
-    value: 5,
-    randomForce: 0.2 + 0.4 * 0.6382086306602428,
-    position: new CANNON.Vec3(0.4, 0.8, 0.6),
-  },
-  {
-    value: 6,
-    randomForce: 0.2 + 0.4 * 0.21318827165314658,
-    position: new CANNON.Vec3(0.4, 0, 0.2),
-  },
-];
-
-let alreadyShown = [];
-let diceRolls = 0;
-
-function resetShown() {
-  alreadyShown = [];
-}
-
 const diceArray = [];
 
 initPhysics();
 initScene();
 
 window.addEventListener("resize", updateSceneSize);
-window.addEventListener("click", triggerDiceThrow);
-
-function triggerDiceThrow() {
-  gravity = true;
-  physicsWorld.gravity.set(0, -50, 0);
-  throwDice();
-}
 
 function initScene() {
   renderer = new THREE.WebGLRenderer({
@@ -205,6 +159,34 @@ function updateSceneSize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+function initDicePosition() {
+  diceArray.forEach((d, dIdx) => {
+    d.body.velocity.setZero();
+    d.body.angularVelocity.setZero();
+
+    d.body.position = new CANNON.Vec3(dIdx === 0 ? -2 : 2, 0, 1);
+    d.mesh.position.copy(d.body.position);
+
+    d.mesh.rotation.set(0, 0, 0);
+    d.body.quaternion.copy(d.mesh.quaternion);
+  });
+}
+
+let alreadyShown = [];
+let diceRolls = 0;
+
+function resetShown() {
+  alreadyShown = [];
+}
+
+window.addEventListener("click", triggerDiceThrow);
+
+function triggerDiceThrow() {
+  gravity = true;
+  physicsWorld.gravity.set(0, -50, 0);
+  throwDice();
+}
+
 function getRandomElement(arr) {
   const copyArr = arr.slice();
 
@@ -219,19 +201,6 @@ function getRandomElement(arr) {
   alreadyShown.push(element.value);
 
   return element;
-}
-
-function initDicePosition() {
-  diceArray.forEach((d, dIdx) => {
-    d.body.velocity.setZero();
-    d.body.angularVelocity.setZero();
-
-    d.body.position = new CANNON.Vec3(dIdx === 0 ? -2 : 2, 0, 1);
-    d.mesh.position.copy(d.body.position);
-
-    d.mesh.rotation.set(0, 0, 0);
-    d.body.quaternion.copy(d.mesh.quaternion);
-  });
 }
 
 function throwDice() {
