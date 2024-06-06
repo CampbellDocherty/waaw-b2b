afcbfa081c7c6a01a91d61e1347786c0fa6ba0f8
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-06-06 20:17:34 +0100

    Unique dj combinations

--- options.js
+++ options.js
@@ -1,40 +1,63 @@
 import * as CANNON from "cannon-es";
 
-export const options = [
+export const firstList = [
   {
-    dj: "abdi",
-    value: 1,
-    randomForce: 0.5 + 0.3 * 0.4532962453133706,
-    position: new CANNON.Vec3(0, 0, 0.2),
+    dj: "albertina",
+    value: 2,
+    randomForce: 0.2 + 0.4 * 0.6195434488172735,
+    position: new CANNON.Vec3(0.4, 0, 0.2),
+    link: "assets/albertina.png",
   },
   {
     dj: "naomi",
     value: 2,
     randomForce: 0.2 + 0.4 * 0.6195434488172735,
     position: new CANNON.Vec3(0.4, 0, 0.2),
+    link: "assets/naomi.png",
   },
   {
     dj: "randy",
     value: 3,
     randomForce: 0.2 + 0.4 * 0.6898916672811655,
     position: new CANNON.Vec3(0.4, 0.8, 0.6),
-  },
-  {
-    dj: "clarisse",
-    value: 4,
-    randomForce: 0.25,
-    position: new CANNON.Vec3(0, 0, 0.2),
+    link: "assets/randy.png",
   },
   {
     dj: "becca",
     value: 5,
     randomForce: 0.2 + 0.4 * 0.6382086306602428,
     position: new CANNON.Vec3(0.4, 0.8, 0.6),
+    link: "assets/becca.png",
+  },
+];
+
+export const secondList = [
+  {
+    dj: "clarisse",
+    value: 4,
+    randomForce: 0.25,
+    position: new CANNON.Vec3(0, 0, 0.2),
+    link: "assets/viriss.png",
   },
   {
     dj: "lc",
     value: 6,
     randomForce: 0.3 + 0.24 * 0.3532962453133706,
     position: new CANNON.Vec3(0.4, 0, 0.2),
+    link: "assets/lc.png",
+  },
+  {
+    dj: "abdi",
+    value: 1,
+    randomForce: 0.5 + 0.3 * 0.4532962453133706,
+    position: new CANNON.Vec3(0, 0, 0.2),
+    link: "assets/abdiablo.png",
+  },
+  {
+    dj: "hiteca",
+    value: 5,
+    randomForce: 0.2 + 0.4 * 0.6382086306602428,
+    position: new CANNON.Vec3(0.4, 0.8, 0.6),
+    link: "assets/hiteca.png",
   },
 ];
