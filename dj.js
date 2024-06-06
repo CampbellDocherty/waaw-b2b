d711ccf6ca70637d698130f7d3ef97b015c180f2
Campbell Docherty <campbell.docherty@oaknorth.co.uk>
2024-06-06 22:27:26 +0100

    Display dj names

--- dj.js
+++ dj.js
@@ -1,15 +1,26 @@
+const fonts = {
+  abdi: "assets/abdi-font.jpg",
+  naomi: "assets/naomi-font.jpg",
+  clarisse: "assets/viriss-font.jpg",
+  becca: "assets/becca-font.jpg",
+  lc: "assets/lc-font.jpg",
+  randy: "assets/randy-font.jpg",
+  albertina: "assets/albertina-font.jpg",
+  hiteca: "assets/hiteca-font.jpg",
+};
+
 export function updateDjName(djOne, djTwo) {
   const elOne = document.getElementById("dj-one");
   const elTwo = document.getElementById("dj-two");
 
-  elOne.textContent = djOne;
-  elTwo.textContent = djTwo;
+  elOne.src = fonts[djOne];
+  elTwo.src = fonts[djTwo];
 }
 
 export function clearDjName() {
   const elOne = document.getElementById("dj-one");
   const elTwo = document.getElementById("dj-two");
 
-  elOne.textContent = "";
-  elTwo.textContent = "";
+  elOne.src = "";
+  elTwo.src = "";
 }
